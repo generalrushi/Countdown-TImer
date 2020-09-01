c491effa3c1ab59f26f28ed918a57d3c649e8c08
Rushikesh Vyavahare <68723846+generalrushi@users.noreply.github.com>
2020-09-01 19:36:16 +0530

    all uploaded

---
+++ app.js
@@ -0,0 +1,40 @@
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 26, 2021 00:00:00").getTime();
+
+// Update the count down every 1 second
+function updateTIme() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="timer"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("timer").innerHTML = "EXPIRED";
+  }
+} ;
+setInterval(updateTIme, 1000);
+
+
+
+/*  ---------yet to Add-------------
+
+function resetTimer() {
+    timer = 60 * 5;
+  }
+document.getElementById("myBtn").addEventListener("click", resetTimer);
+
+*/   
